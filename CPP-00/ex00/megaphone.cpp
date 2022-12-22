9ada26ed91709011cc09cd1b2e6b733e06c14bb2
aysuoner <aysuoner5@gmail.com>
2022-12-22 14:24:45 +0300

    first commit

---
+++ CPP-00/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using std:: cout;
+using std:: endl;
+
+int main(int argc, char **argv)
+{
+	int i;
+	int k;
+
+	i = 1;
+	if (argc >=  2)
+	{
+		while (argv[i])
+		{
+			k = 0;
+			while (argv[i][k])
+			{
+				if (argv[i][k] >= 97 && argv[i][k] <= 122)
+					argv[i][k] -= 32;
+				cout << argv[i][k];
+				k++;
+			}
+			i++;
+		}
+	}
+
+	else if (argc == 1)
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	cout << endl;
+}
+
+/* deneneeme */
