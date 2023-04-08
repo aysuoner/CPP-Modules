9157bb135d847f9ad365b05100b2c09b5b267fd4
aysuoner <aysuoner5@gmail.com>
2023-04-08 20:55:50 +0300

    cpp07 first commit

---
+++ CPP-07/ex00/main.cpp
@@ -0,0 +1,37 @@
+#include "whatever.hpp"
+
+int main( void )
+{
+	std::cout << "\ntest int" << std::endl;
+	int a = 2;
+	int b = 3;
+	::swap<int>( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::cout << "\ntest string " << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	std::cout << "\ntest float" << std::endl;
+	float a1 = 3.46;
+	float b1 = 2;
+	::swap(a1,b1);
+	std::cout << "a = " << a1 << ", b = " << b1 << std::endl;
+	::swap(a1,b1);
+	std::cout << "a = " << a1 << ", b = " << b1 << std::endl;
+
+	double a2 = 3.14;
+	double b2 = 2.71828;
+	std::cout << "\ntest double" << std::endl;
+	::swap(a2,b2);
+	std::cout << "a = " << a2 << ", b = " << b2 << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a2, b2 ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a2, b2 ) << std::endl;
+	return 0;
+}
---
+++ CPP-07/ex00/whatever.hpp
@@ -0,0 +1,28 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+void swap(T &src1, T &src2)
+{
+	T temp;
+	temp = src2;
+	src2 = src1;
+	src1 = temp;
+}
+
+template <typename T>
+T max(T &src1, T &src2)
+{
+	return( src1 > src2 ? src1 : src2);
+}
+
+template <typename T>
+T min(T &src1, T &src2)
+{
+	if (src1 < src2 ? src1 : src2)
+}
+
+#endif
