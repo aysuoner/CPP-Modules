7dc66aec7befbf21c19b89653c1571372bf770b7
aysuoner <aysuoner5@gmail.com>
2023-04-09 14:03:26 +0300

    cpp07 ex02 finito

---
+++ CPP-07/ex02/main.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoner <aoner@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/08 22:11:24 by aoner             #+#    #+#             */
+/*   Updated: 2023/04/09 13:58:16 by aoner            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+/* #define SIZE 10
+
+int main( void )
+{
+    std::cout << "-----------------------------------------------------" << std::endl;
+
+    Array< float > test;
+
+    Array< int > intArray( SIZE );
+    Array< int > intArray2( SIZE - 5 );
+
+    for ( unsigned int i = 0; i < intArray.size(); i++ )
+        intArray[i] = i * 2;
+
+    std::cout << "Int Array 1: " << intArray << std::endl;
+
+    intArray2 = intArray;
+
+    std::cout << "int Array 2: " << intArray2 << std::endl;
+
+    try {
+        std::cout << "Accessing a valid index: " << intArray[5] << std::endl;
+        std::cout << "Accessing an invalid index: " << intArray[SIZE] << std::endl;
+    } catch ( Array< int >::OutOfBoundsException& e ) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+    std::cout << "-----------------------------------------------------" << std::endl;
+
+    return ( 0 );
+} */
+
+int main(void)
+{
+    Array<int> a(10);
+    
+    try
+    {
+        a[6] = 8;
+        std::cout << a[6] << std::endl;
+        std::cout << a[50] << std::endl;
+    }
+    catch(std::exception &e)
+    {
+        std::cout << "index is out of range" << std::endl;
+    }
+    return (0);
+}
