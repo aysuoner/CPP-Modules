9d67e76ab8d049ae86b8aafed1b3fe06cdd695ea
aysuoner <aysuoner5@gmail.com>
2023-04-09 22:36:24 +0300

    ex00 first commit

--- CPP-08/ex00/easyfind.hpp
+++ CPP-08/ex00/easyfind.hpp
@@ -7,5 +7,45 @@
 #include <iostream>
 #include <vector>
 
-template <typename T>
+class NotFoundException: public std::exception
+{
+	public:
+			virtual const char *what() const throw()
+			{
+				return ("Not found");
+			}
+};
+
+/* typename T::iterator şeklinde belirtilen fonksiyonun geri dönüş değeri,
+T türünün hangi konteyner türü olduğundan bağımsızdır.
+Çünkü birçok konteyner türü, ::iterator türüne sahip olduğu için,
+typename T::iterator türü belirli bir türü ifade etmek yerine, bir tür ailesini ifade eder.
+Bu nedenle, çağıran kod tarafından geri dönüş değeri türü belirtilmesine gerek yoktur
+ve typename T::iterator türü doğru şekilde tanımlanmışsa, fonksiyon kullanıldığı konteyner türü
+ne olursa olsun doğru şekilde çalışacaktır.
+u nedenle, easyfind fonksiyonuna bir tür belirtilmediği takdirde, container parametresinin hangi türde olduğu belirsiz kalacaktır.
+Bu belirsizlik, çağıran kod tarafından tür belirtilmedikçe, hata veya yanlış sonuçlara neden olabilir.*/
+
+/* std::find fonksiyonu, verilen değeri içeren ilk öğenin işaretçisini döndürür.
+Bu işaretçi, konteynerin sonunu gösteren işaretçiyle eşitse, değer bulunamamıştır.
+Örneğin, easyfind fonksiyonu, STL vector, list veya set gibi birçok farklı konteyner türü için çalışabilir.
+Ancak, kullanıcı tarafından tür belirtilmediğinde, container parametresinin türü belirsiz kalacaktır.
+Bu durumda, bir std::vector kullanıcı olarak düşünebiliriz.
+Ancak, container parametresinin türü belirtilmediği için, easyfind fonksiyonu, çağrıldığı türün begin() ve end() fonksiyonlarını 
+çağırmaya çalışırken bir hata verebilir veya yanlış sonuçlar döndürebilir.
+Örneğin, eğer container parametresi aslında bir std::list ise,
+std::vector için optimize edilmiş algoritmayı kullanmaya çalışarak hatalı sonuçlar üretebilir.
+Bu nedenle, easyfind fonksiyonunun kullanılacağı türün açıkça belirtilmesi önemlidir.*/
+template< typename T>
+typename T::iterator easyfind(T &in, int i)
+{
+	typename T::iterator	it;
+	it = find(in.begin(), in.end(), i);
+	if (it == in.end())
+	{
+		throw (NotFoundException());
+	}
+	return (it);
+}
+
 #endif
--- CPP-08/ex00/main.cpp
+++ CPP-08/ex00/main.cpp
@@ -6,12 +6,49 @@
 /*   By: aoner <aoner@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 15:58:09 by aoner             #+#    #+#             */
-/*   Updated: 2023/04/09 18:13:48 by aoner            ###   ########.fr       */
+/*   Updated: 2023/04/09 22:29:26 by aoner            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
-int main(){
+#include "easyfind.hpp"
+#include <list>
 
+int main()
+{
+	std::list<int> test;
+	test.push_back(155);
+	test.push_back(156);
+	test.push_back(157);
+
+	std::list<int>::const_iterator	it = test.end();
+
+	try
+	{
+		it = ::easyfind(test, 155);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	if (it != test.end())
+		std::cout << *it << " found" << std::endl;
+
+	it = test.end();
+
+	try
+	{
+		it = ::easyfind(test, 15);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	if (it != test.end())
+		std::cout << *it << " found" << std::endl;
+
+	return (0);
 }
